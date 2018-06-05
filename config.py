a246827ce6f132798c271305b7317becd6678428
Hai Van <hvandang@outlook.com>
2018-06-05 13:03:49 +0100

    Retrieve the option of sending mail for resetting password from enviromental environment

--- config.py
+++ config.py
@@ -25,12 +25,15 @@ class Config(object):
 		'sqlite:///' + os.path.join(basedir, 'credential.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+	PROVISION_FILE = os.environ.get('PROVISION_FILE') or os.path.join(basedir, 'provisioning.csv')
+
 	# Mail configuration
 	MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
 	MAIL_USE_TLS = int(os.environ.get('MAIL_USE_TLS') or 1)is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your_email@gmail.com' # from address
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'your_email_password' # fill in the password here
+	MAIL_SEND_RESET_PWD = = os.environ.get('MAIL_SEND_RESET_PWD') or False
 	ADMINS = ['your_email@gmail.com'] # to address
 
 #DATABASE_CONFIG = {
